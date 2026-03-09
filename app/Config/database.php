6a4a052d0557b5af12383fa02f0d370502aa2357
ijnavas <nacho@manadi.es>
2026-03-09 21:27:20 +0100

    cambio en base de datos

--- app/Config/database.php
+++ app/Config/database.php
@@ -4,8 +4,8 @@ declare(strict_types=1);
 return [
     'host' => env('DB_HOST', '127.0.0.1'),
     'port' => env('DB_PORT', '3306'),
-    'database' => env('DB_DATABASE', 'crm_tpt'),
-    'username' => env('DB_USERNAME', 'root'),
-    'password' => env('DB_PASSWORD', ''),
+    'database' => env('DB_DATABASE', 'manad_tpt'),
+    'username' => env('DB_USERNAME', 'manad_tpt'),
+    'password' => env('DB_PASSWORD', 'zATwG92]3;7@'),
     'charset' => 'utf8mb4',
 ];
