c17c30c4fbe9ddf9d237b827790d84aa64f0c510
ijnavas <nacho@manadi.es>
2026-03-10 21:51:24 +0100

    BIEN

--- public/index.php
+++ public/index.php
@@ -1,13 +1,6 @@
 <?php
 declare(strict_types=1);
 
-ini_set('display_errors', '1');
-ini_set('display_startup_errors', '1');
-error_reporting(E_ALL);
-
-
-
-
 use App\Core\App;
 
 define('BASE_PATH', dirname(__DIR__));
