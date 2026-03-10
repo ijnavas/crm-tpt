c1281697bdc5b4330056b1ccec55f3edf774377e
ijnavas <nacho@manadi.es>
2026-03-10 22:11:12 +0100

    rutas

--- app/Config/routes.php
+++ app/Config/routes.php
@@ -4,6 +4,9 @@ declare(strict_types=1);
 use App\Controllers\AuthController;
 use App\Controllers\DashboardController;
 use App\Controllers\LeadController;
+use App\Controllers\CompanyController;
+use App\Controllers\ContactController;
+use App\Controllers\TaskController;
 
 return [
     ['GET', '/', [AuthController::class, 'loginForm']],
@@ -21,4 +24,27 @@ return [
     ['POST', '/leads/{id}/update', [LeadController::class, 'update']],
     ['POST', '/leads/{id}/notes', [LeadController::class, 'storeNote']],
     ['POST', '/leads/{id}/status', [LeadController::class, 'updateStatus']],
+['GET', '/companies', [CompanyController::class, 'index']],
+['GET', '/companies/create', [CompanyController::class, 'create']],
+['POST', '/companies/store', [CompanyController::class, 'store']],
+['GET', '/companies/{id}', [CompanyController::class, 'show']],
+['GET', '/companies/{id}/edit', [CompanyController::class, 'edit']],
+['POST', '/companies/{id}/update', [CompanyController::class, 'update']],
+
+['GET', '/contacts', [ContactController::class, 'index']],
+['GET', '/contacts/create', [ContactController::class, 'create']],
+['POST', '/contacts/store', [ContactController::class, 'store']],
+['GET', '/contacts/{id}', [ContactController::class, 'show']],
+['GET', '/contacts/{id}/edit', [ContactController::class, 'edit']],
+['POST', '/contacts/{id}/update', [ContactController::class, 'update']],
+
+['GET', '/tasks', [TaskController::class, 'index']],
+['GET', '/tasks/create', [TaskController::class, 'create']],
+['POST', '/tasks/store', [TaskController::class, 'store']],
+['GET', '/tasks/{id}', [TaskController::class, 'show']],
+['GET', '/tasks/{id}/edit', [TaskController::class, 'edit']],
+['POST', '/tasks/{id}/update', [TaskController::class, 'update']],
+['POST', '/tasks/{id}/complete', [TaskController::class, 'complete']],
+
+
 ];
