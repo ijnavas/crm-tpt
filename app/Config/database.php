c6468a66a4da30a7a306f373f5c7d862f8b1040a
ijnavas <nacho@manadi.es>
2026-03-10 21:22:40 +0100

    db

--- app/Config/database.php
+++ app/Config/database.php
@@ -2,7 +2,8 @@
 declare(strict_types=1);
 
 return [
-    'host' => env('DB_HOST', 'localhost'),
+    'host' => env('DB_HOST', '127.0.0.1'),
+    'port' => env('DB_PORT', '3306'),
     'database' => env('DB_DATABASE', 'manad_tpt'),
     'username' => env('DB_USERNAME', 'manad_tpt'),
     'password' => env('DB_PASSWORD', 'zATwG92]3;7@'),
