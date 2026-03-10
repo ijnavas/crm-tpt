97207e7a0b8e9534b16513fc5848f2b14c5c496c
ijnavas <nacho@manadi.es>
2026-03-10 21:07:14 +0100

    cambio puerto y localhost

--- app/Config/database.php
+++ app/Config/database.php
@@ -2,8 +2,7 @@
 declare(strict_types=1);
 
 return [
-    'host' => env('DB_HOST', '127.0.0.1'),
-    'port' => env('DB_PORT', '3306'),
+    'host' => env('DB_HOST', 'localhost'),
     'database' => env('DB_DATABASE', 'manad_tpt'),
     'username' => env('DB_USERNAME', 'manad_tpt'),
     'password' => env('DB_PASSWORD', 'zATwG92]3;7@'),
